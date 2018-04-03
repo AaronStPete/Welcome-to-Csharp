cd04286de10f93caa22c1a736feb4334f5c0989a
AaronStPete <AaronStPete@users.noreply.github.com>
2018-04-03 13:42:31 -0400

    Add project files.

---
+++ Welcome-to-Csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Welcome_to_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
